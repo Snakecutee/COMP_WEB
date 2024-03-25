beeb6f824fd899af18ed525804c36df94841bfd0
toicusin3 <mr.rakenraj@gmail.com>
2024-03-25 22:11:08 +0700

    delete account

---
+++ front_end/src/store/saga/index.js
@@ -0,0 +1,7 @@
+import { all } from "redux-saga/effects";
+
+import authenticateSaga from "./auth.saga";
+import subRouterSaga from "./subRouter.saga";
+export default function* rootSaga() {
+  yield all([authenticateSaga(), subRouterSaga()]);
+}
